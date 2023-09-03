7ab994f61add566c25c0b5cbb68ec8d5f2910540
martinachov <martin1082v@gmail.com>
2023-09-03 19:56:54 -0300

    Circuit Breaker

---
+++ resilience4j/src/main/java/com/martinachov/resilience4j/dto/Response.java
@@ -0,0 +1,14 @@
+package com.martinachov.resilience4j.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+
+    private String data;
+    private int code;
+}
